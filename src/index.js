d72832e85467cafb3fd14e97b45c78d1422a6a06
Artem Mikaelyan <Fanateshen@gmail.com>
2023-08-05 15:41:12 +0300

    feat: add decision of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let converter = require('number-to-words');
+    converter = converter.toWords(number);
+    return converter.replace(/-/g, ' ');
 }
